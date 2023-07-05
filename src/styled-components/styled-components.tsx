230e330c51c03a6aa039eae9a7f615c28bd3a0fa
sunmin <dlatjsals123@naver.com>
2023-07-06 04:19:37 +0900

    style: 장바구니 페이지 css 추가

--- src/styled-components/styled-components.tsx
+++ src/styled-components/styled-components.tsx
@@ -1,12 +1,20 @@
 "use client";
 import styled from "@emotion/styled";
 
-/* 상품 상세페이지 */
-export const DetailPageRoot = styled.div`
+export const ColumnFlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const FlexDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: calc(100vh - 76px);
+`;
+
+/* 상품 상세페이지 */
+export const DetailPageRoot = styled(FlexDiv)`
   > div {
     width: 500px;
   }
@@ -41,24 +49,63 @@ export const ProductPrice = styled.p`
   margin-bottom: 50px;
   text-align: end;
 `;
-
-export const CartButton = styled.button`
-  width: 100%;
+export const Button = styled.button`
   background-color: beige;
-  padding: 15px 0px;
+  padding: 15px 30px;
   border-radius: 8px;
 `;
+export const SmallButton = styled.button`
+  background-color: beige;
+  padding: 10px;
+  border-radius: 8px;
+`;
+export const CartButton = styled(Button)`
+  width: 100%;
+`;
 
-export const CountInput = styled.input`
+/* 수량 변경 Input */
+export const AmountCounterWrapper = styled.div<{ isShoppingBasket: boolean }>`
+  width: ${(props) => (props.isShoppingBasket ? "150px" : "")};
+  display: ${(props) => (props.isShoppingBasket ? "flex" : "")};
+  justify-content: ${(props) => (props.isShoppingBasket ? "center" : "")};
+`;
+
+export const CountInput = styled.input<{ isShoppingBasket: boolean }>`
   border: 2px solid lightgray;
-  height: 36px;
-  width: 60px;
+  height: ${(props) => (props.isShoppingBasket ? "26px" : "36px")};
+  width: ${(props) => (props.isShoppingBasket ? "40px" : "60px")};
   text-align: center;
 `;
 
-export const MiniButton = styled.button`
+export const MiniButton = styled.button<{ isShoppingBasket: boolean }>`
   background-color: lightgray;
-  margin-bottom: 20px;
-  height: 40px;
-  width: 40px;
+  margin-bottom: ${(props) => (props.isShoppingBasket ? "0px" : "20px")};
+  height: ${(props) => (props.isShoppingBasket ? "30px" : "40px")};
+  width: ${(props) => (props.isShoppingBasket ? "30px" : "40px")};
+`;
+
+/* 장바구니 페이지 */
+export const TableHead = styled.div`
+  display: flex;
+  align-items: center;
+  border-top: 1px solid gray;
+  border-bottom: 1px solid gray;
+  padding: 20px 0px;
+  width: fit-content;
+`;
+export const CheckBox = styled.input`
+  width: 100px;
+  text-align: center;
+`;
+export const SmallParagraph = styled.p`
+  width: 100px;
+  text-align: center;
+`;
+export const MediumParagraph = styled.p`
+  width: 160px;
+  text-align: center;
+`;
+export const LargeParagraph = styled.p`
+  width: 400px;
+  text-align: center;
 `;
