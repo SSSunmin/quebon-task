04f51e3267d489ec396cb0588a6955ae534f4a05
sunmin <dlatjsals123@naver.com>
2023-07-06 12:50:58 +0900

    fix: 여러개 물품이 삭제될 때 물품 목록이 삭제되지 않고 복사되는 오류 수정

--- src/hooks/useDeleteList.ts
+++ src/hooks/useDeleteList.ts
@@ -1,16 +1,20 @@
-import { CartInfo, CartProps } from "@/RecoilState";
+import { CartInfo, CartProps, SelectProduct } from "@/RecoilState";
 import { useRecoilState } from "recoil";
 
 const useDeleteList = () => {
   const [shoppingBasket, setShoppingBasket] = useRecoilState(CartInfo);
+  const [selected, setselected] = useRecoilState(SelectProduct);
 
   const DeleteShoppingBasket = (id: number[]) => {
     const newList: CartProps[] = [];
     shoppingBasket.map((data) => {
-      id.map((d) => (d !== data.id ? newList.push(data) : ""));
+      if (id.indexOf(data.id) === -1) {
+        newList.push(data);
+      }
     });
 
     setShoppingBasket(newList);
+    setselected([]);
   };
   return DeleteShoppingBasket;
 };
