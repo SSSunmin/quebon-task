1634d68a9c416556abee1d0a029a62b197ea865b
sunmin <dlatjsals123@naver.com>
2023-07-06 21:44:42 +0900

    chore: 홈 화면 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>WELCOME!!</div>
+      <img
+        src="https://www.whatsonincologne.com/wp-content/uploads/2017/05/fashion_shopping.jpg"
+        alt="mainimg"
+      />
+    </main>
+  );
 }
